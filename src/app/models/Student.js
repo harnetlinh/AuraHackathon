ff019bf304a71ded726ffcf90cff1575b5a3cba5
Ha Ngoc Linh <harnet.linh@gmail.com>
2022-07-12 20:51:37 +0700

    update mongoose

--- src/app/models/Student.js
+++ src/app/models/Student.js
@@ -1,37 +1,68 @@
 import mongoose from "../../config/mongo";
 
-const Student = mongoose.model("Student", {
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    studentCode: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    nftAddress: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    prizeCollection: {
-        type: Array,
-        required: true,
-        default: [],
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now,
-    },
+const PrizeSchema = new mongoose.Schema(
+    {
+        prizeName: {
+            type: String,
+            required: true
+        },
+        prizeDescription: {
+            type: String,
+            required: false
+        },
+        prizeImage: {
+            type: String,
+            required: false
+        },
+        prizeCategory: {
+            type: String,
+            required: false
+        },
+        prizeDate: {
+            type: Date,
+            required: false,
+            default: Date.now
+        },
+        CID: {
+            type: String,
+            required: true
+        },
+        statusID: {
+            type: Number,
+            required: true,
+            default: 0
+        }
+    }, 
+    { strict: false },
+    { timestamps: true });
 
-});
+const StudentSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        studentCode:{
+            type: String,
+            required: true
+        },
+        studentLogin: {
+            type: String,
+            required: true
+        },
+        nftAddress:{
+            type: String,
+            required: true
+        },
+        prizeCollection: [PrizeSchema],
+    },
+    { strict: false },
+    { timestamps: true }
+);
+
+const Student = mongoose.model("Student", StudentSchema);
+export default Student;
