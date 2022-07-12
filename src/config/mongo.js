0c31f9c264db049ca86b45eda2f655dece519fce
tuhd4 <tuhd4@fpt.edu.vn>
2022-07-13 00:02:32 +0700

    fix connect mongodb

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connect = async () => {
-    await mongoose.connect(`mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/?authMechanism=DEFAULT`, {
+    await mongoose.connect(`mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}?authMechanism=DEFAULT`, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then((con) => {
