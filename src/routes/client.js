2b6f80f12291a63665ba57fae239bc350fc84d79
Ha Ngoc Linh <harnet.linh@gmail.com>
2022-07-13 21:38:59 +0700

    update api upload image

--- src/routes/client.js
+++ src/routes/client.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { addWallet } from "../app/controllers/user.controller.js";
+import { uploadImageToWeb3 } from "../app/controllers/web3Storage.js";
 const router = express.Router();
 
 router.post("/add-wallet", addWallet);
+router.post("/upload-image-web3storage", uploadImageToWeb3);
 
 export default router;
