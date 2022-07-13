febf47d2f5d422ce432d4a0890784a73cfc184c9
Ha Ngoc Linh <harnet.linh@gmail.com>
2022-07-13 22:29:23 +0700

    update get all token

--- src/app/controllers/aura.controller.js
+++ src/app/controllers/aura.controller.js
@@ -30,6 +30,11 @@ const getSigningAuraWasmClient = async (wallet) => {
     return signingClient;
 };
 
+const getAuraWasmClient = async () => {
+    const client = await CosmWasmClient.connect(rpcEndpoint);
+    return client;
+  }
+
 const MintNFT = async (req, res) => {
     let wallet = await getWallet();
 
@@ -50,7 +55,7 @@ const MintNFT = async (req, res) => {
         amount: [
             {
                 denom: "uaura",
-                amount: "1",
+                amount: "160",
             },
         ],
         gas: "152375",
@@ -75,4 +80,34 @@ const MintNFT = async (req, res) => {
     }
 };
 
-export { MintNFT };
+const getAllToken = async (req, res) => {
+    /* 	#swagger.tags = ['All Token']
+    #swagger.description = 'Get all NFT Token' */
+  
+    if (!client) {
+      client = await getAuraWasmClient();
+    }
+    console.log(req.params.address);
+    const allTokenOwner = {
+      tokens: {
+        owner: String(req.params.address),
+      },
+    };
+    try {
+      const result = await client.queryContractSmart(
+        contractAddress,
+        allTokenOwner
+      );
+      res.status(200).json({
+        data: [result],
+        message: "Found Result",
+      });
+    } catch (err) {
+      res.status(500).json({
+        data: [err.message],
+        message: "Error",
+      });
+    }
+  };
+
+export { MintNFT, getAllToken };
